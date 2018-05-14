b1d8c229e8c36852a54b31e95c8cba0c4ddb77f4
Andrew Parmar <andrew.parmar@gmail.com>
2018-05-14 00:37:27 -0700

    Added user profile and posts detail functionality

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '858d2251646011ba1936bf8ba6600f12'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 api = Api(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
